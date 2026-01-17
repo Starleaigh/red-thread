592861be83548bf01a5aa7d0a5b1dd4735ab49bb
Starleaigh <255539381+Starleaigh@users.noreply.github.com>
2026-01-17 12:36:50 -0600

    Fix download url and add download url change to version updae code

--- update-version.js
+++ update-version.js
@@ -6,7 +6,20 @@ const version = packageJson.version;
 
 const updateFile = (path) => {
     const data = JSON.parse(fs.readFileSync(path, "utf8"));
+
+    //update version
     data.version = version;
+
+    // Update download URL (if present)
+    if (data.download) {
+    data.download = `https://github.com/Starleaigh/red-thread/archive/refs/tags/v${version}.zip`;
+    }
+
+    // Update manifest URL (if present)
+    if (data.manifest) {
+    data.manifest = `https://raw.githubusercontent.com/Starleaigh/red-thread/main/manifest.json`;
+    }
+ 
     fs.writeFileSync(path, JSON.stringify(data, null, 2));
 };
 
