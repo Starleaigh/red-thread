03a788bb544f8da00389f665d1bee0760ffc3e53
Starleaigh <255539381+Starleaigh@users.noreply.github.com>
2026-01-17 21:57:13 -0600

    Added PinManager as the basis of a node system for threads to attach to

--- src/module.js
+++ src/module.js
@@ -2,7 +2,6 @@
 // module.js
 
 import { registerHooks } from "./hooks.js";
-import { drawThread, redrawThread } from "./threads.js";
 
 // Notifies when the module is initialized
 Hooks.once("init", () => {
@@ -15,17 +14,3 @@ Hooks.once("ready", () =>{
   registerHooks();
 });
 
-Hooks.on("canvasReady", () => {
-  console.log("Red Thread | Canvas Ready (Module.js)");
-
-  // TEMP grab first two tokens on the canvas
-  const tokens = canvas.tokens.placeables;
-
-  if (tokens.length >= 2) {
-    drawThread(tokens[0], tokens[1]);
-  }
-});
-
-Hooks.on("updateToken", () => {
-  redrawThread();
-});
